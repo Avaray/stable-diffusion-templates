6e8413010928186686ee2f22434dca47bff75e34
Avaray <1459553+Avaray@users.noreply.github.com>
2024-10-24 10:08:54 +0200

    created ratings file

---
+++ data/ratings.ts
@@ -0,0 +1,11 @@
+interface Rating {
+  [key: string]: [string, string];
+}
+
+export const ratings: Rating = {
+  u: ['⏳', 'Needs more testing.'],
+  a: ['🔥', "It's fire!"],
+  b: ['👍', "It's OK."],
+  c: ['👎', "It's bad. Will be deleted probably."],
+  d: ['💩', "It's crap"],
+};
