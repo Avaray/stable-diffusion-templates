0a289837aa0096621f4cc160913e80f2b4f83de5
Avaray <1459553+Avaray@users.noreply.github.com>
2024-10-24 10:59:13 +0200

    created extensions list

---
+++ data/extensions.ts
@@ -0,0 +1,33 @@
+export interface Extension {
+  name: string;
+  uis: string[];
+  base: string[];
+  url: string;
+}
+
+export const extensions: Extension[] = [
+  {
+    name: 'Dynamic Prompts',
+    uis: ['forge'],
+    base: ['sdxl', 'pdxl'],
+    url: 'https://github.com/adieyal/sd-dynamic-prompts',
+  },
+  {
+    name: 'ADetailer',
+    uis: ['forge'],
+    base: ['sdxl', 'pdxl'],
+    url: 'https://github.com/Bing-su/adetailer',
+  },
+  {
+    name: 'Clip Interrogator',
+    uis: ['forge'],
+    base: ['sdxl'],
+    url: 'https://github.com/pharmapsychotic/clip-interrogator',
+  },
+  {
+    name: 'WD14 Tagger',
+    uis: ['forge'],
+    base: ['pdxl'],
+    url: 'https://github.com/picobyte/stable-diffusion-webui-wd14-tagger',
+  },
+];
