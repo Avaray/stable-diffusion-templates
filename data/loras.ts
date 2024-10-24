68d05cda84338d8cb577ad7fc3359b8fc5746103
Avaray <1459553+Avaray@users.noreply.github.com>
2024-10-24 10:32:00 +0200

    added pretty eyes lora

--- data/loras.ts
+++ data/loras.ts
@@ -43,6 +43,13 @@ export const loras = [
     triggerwords: [],
   },
   {
+    name: 'Envy Pony Pretty Eyes',
+    filename: 'EnvyPonyPrettyEyes01.safetensors',
+    homepage: 'https://civitai.com/models/393101/envy-pony-pretty-eyes-01-pretty-anime-eyes',
+    base: ['pdxl'],
+    triggerwords: [],
+  },
+  {
     name: 'GLSHS Style v2.4',
     filename: 'GLSHS_Style_V2_4.safetensors',
     homepage: 'https://civitai.com/models/550871?modelVersionId=669776',
