95ebb173074a03d61450f34e7ce9b46e9683c37f
Avaray <1459553+Avaray@users.noreply.github.com>
2024-11-28 22:14:40 +0100

    added api key

--- index.ts
+++ index.ts
@@ -103,6 +103,13 @@ const vastaiInstall = await executeCommand("pip", [
   "vastai",
 ]);
 
+// Add API key to VastAI
+const vastaiAddKey = await executeCommand("vastai", [
+  "set",
+  "api-key",
+  vastaiApiKey,
+]);
+
 // Iterate over all UIs
 for (const ui of uis) {
   // Currently it's just a shebang
