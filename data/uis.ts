b1f4fe836d8bf5cc563ba7d65aafc38a05593dfb
Avaray <1459553+Avaray@users.noreply.github.com>
2024-12-19 21:55:43 +0100

    Update uis.ts

--- data/uis.ts
+++ data/uis.ts
@@ -16,7 +16,7 @@ const defaultPvsUrl = (repository: string) => {
 };
 
 const universalEnv =
-  `-e WEB_ENABLE_HTTPS=true -e DATA_DIRECTORY=/workspace/ -e WORKSPACE=/workspace/ -e WORKSPACE_MOUNTED=force -e JUPYTER_DIR=/ -e SYNCTHING_TRANSPORT_PORT_HOST=72299 -e OPEN_BUTTON_TOKEN=1 -e OPEN_BUTTON_PORT=1111 -p 22:22 -p 1111:1111 -p 7860:7860 -p 8384:8384 -p 8888:8888 -p 72299:72299`;
+  `-e WEB_ENABLE_HTTPS=true -e DATA_DIRECTORY=/workspace/ -e WORKSPACE=/workspace/ -e WORKSPACE_MOUNTED=force -e JUPYTER_DIR=/ -e SYNCTHING_TRANSPORT_PORT_HOST=72299 -e OPEN_BUTTON_TOKEN=1 -e OPEN_BUTTON_PORT=1111 -p 22:22 -p 1111:1111 -p 8384:8384 -p 8888:8888 -p 72299:72299`;
 
 const userInterfaces: UI[] = [
   {
@@ -28,41 +28,41 @@ const userInterfaces: UI[] = [
     supports: ["sdxl", "pdxl"],
     flags: "",
     diskSpace: 40,
-    env: `${universalEnv} -e FORGE_ARGS=""`,
+    env: `${universalEnv} -e FORGE_ARGS="" -e FORGE_PORT_HOST=7860 -p 7860:7860`,
   },
-  {
-    id: "comfy",
-    name: "Comfy",
-    image: "ghcr.io/ai-dock/comfyui",
-    repository: "https://github.com/ai-dock/comfyui",
-    pvs: "",
-    supports: ["sdxl", "pdxl"],
-    flags: "",
-    diskSpace: 40,
-    env: `${universalEnv} -e COMFYUI_ARGS="" -e COMFYUI_PORT_HOST=8188 -p 8188:8188`,
-  },
-  {
-    id: "invoke",
-    name: "Invoke",
-    image: "ghcr.io/ai-dock/invokeai",
-    repository: "https://github.com/ai-dock/invokeai",
-    pvs: "",
-    supports: ["sdxl", "pdxl"],
-    flags: "",
-    diskSpace: 40,
-    env: `${universalEnv} -e INVOKEAI_PORT_HOST=9090 -p 9090:9090`,
-  },
-  {
-    id: "fooocus",
-    name: "Fooocus",
-    image: "ghcr.io/ai-dock/fooocus",
-    repository: "https://github.com/ai-dock/fooocus",
-    pvs: "",
-    supports: ["sdxl", "pdxl"],
-    flags: "",
-    diskSpace: 40,
-    env: `${universalEnv} -e FOOOCUS_ARGS="--disable-preset-download --disable-analytics" -e FOOOCUS_PORT_HOST=7865 -p 7865:7865`,
-  },
+  // {
+  //   id: "comfy",
+  //   name: "Comfy",
+  //   image: "ghcr.io/ai-dock/comfyui",
+  //   repository: "https://github.com/ai-dock/comfyui",
+  //   pvs: "",
+  //   supports: ["sdxl", "pdxl"],
+  //   flags: "",
+  //   diskSpace: 40,
+  //   env: `${universalEnv} -e COMFYUI_ARGS="" -e COMFYUI_PORT_HOST=8188 -p 8188:8188`,
+  // },
+  // {
+  //   id: "invoke",
+  //   name: "Invoke",
+  //   image: "ghcr.io/ai-dock/invokeai",
+  //   repository: "https://github.com/ai-dock/invokeai",
+  //   pvs: "",
+  //   supports: ["sdxl", "pdxl"],
+  //   flags: "",
+  //   diskSpace: 40,
+  //   env: `${universalEnv} -e INVOKEAI_PORT_HOST=9090 -p 9090:9090`,
+  // },
+  // {
+  //   id: "fooocus",
+  //   name: "Fooocus",
+  //   image: "ghcr.io/ai-dock/fooocus",
+  //   repository: "https://github.com/ai-dock/fooocus",
+  //   pvs: "",
+  //   supports: ["sdxl", "pdxl"],
+  //   flags: "",
+  //   diskSpace: 40,
+  //   env: `${universalEnv} -e FOOOCUS_ARGS="--disable-preset-download --disable-analytics" -e FOOOCUS_PORT_HOST=7865 -p 7865:7865`,
+  // },
 ];
 
 async function fetchPvsData(url: string): Promise<string> {
