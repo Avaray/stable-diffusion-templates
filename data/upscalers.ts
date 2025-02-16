0ba09264d3feac4076e357be181755c6910380b5
Avaray <1459553+Avaray@users.noreply.github.com>
2025-02-16 10:20:28 +0100

    Update upscalers.ts

--- data/upscalers.ts
+++ data/upscalers.ts
@@ -3,25 +3,29 @@ export interface Upscaler {
   filename: string;
   homepage: string;
   base: string[];
+  comment?: string;
 }
 
 export const upscalers: Upscaler[] = [
   {
-    name: '4x UltraSharp',
-    filename: '4x-UltraSharp.pth',
-    homepage: '',
-    base: ['sdxl', 'pdxl'],
+    name: "4x UltraSharp",
+    filename: "4x-UltraSharp.pth",
+    homepage: "",
+    base: ["sdxl", "pdxl"],
+    comment: "",
   },
   {
-    name: '8x NMKD Superscale 150000 G',
-    filename: '8x_NMKD-Superscale_150000_G.pth',
-    homepage: '',
-    base: ['sdxl'],
+    name: "8x NMKD Superscale 150000 G",
+    filename: "8x_NMKD-Superscale_150000_G.pth",
+    homepage: "",
+    base: ["sdxl"],
+    comment: "",
   },
   {
-    name: '2xHFA2kOmniSR',
-    filename: '2xHFA2kOmniSR.pth',
-    homepage: 'https://huggingface.co/Phips/2xHFA2kOmniSR',
-    base: ['pdxl'],
+    name: "2xHFA2kOmniSR",
+    filename: "2xHFA2kOmniSR.pth",
+    homepage: "https://huggingface.co/Phips/2xHFA2kOmniSR",
+    base: ["pdxl"],
+    comment: "",
   },
 ];
