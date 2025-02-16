f3e5c49c7f7bd592ab3164522e40e4ef7001ea79
Avaray <1459553+Avaray@users.noreply.github.com>
2025-02-16 13:19:05 +0100

    Update extensions.ts

--- data/extensions.ts
+++ data/extensions.ts
@@ -5,35 +5,31 @@ export interface Extension {
   url: string;
 }
 
+// https://github.com/AUTOMATIC1111/stable-diffusion-webui-extensions/blob/extensions/index.json
+
 export const extensions: Extension[] = [
   {
-    name: 'Dynamic Prompts',
-    uis: ['forge'],
-    base: ['sdxl', 'pdxl'],
-    url: 'https://github.com/adieyal/sd-dynamic-prompts',
+    name: "Dynamic Prompts",
+    uis: ["forge"],
+    base: ["sdxl", "pdxl"],
+    url: "https://github.com/adieyal/sd-dynamic-prompts",
   },
   {
-    name: 'ADetailer',
-    uis: ['forge'],
-    base: ['sdxl', 'pdxl'],
-    url: 'https://github.com/Bing-su/adetailer',
+    name: "ADetailer",
+    uis: ["forge"],
+    base: ["sdxl", "pdxl"],
+    url: "https://github.com/Bing-su/adetailer",
   },
   {
-    name: 'LoRA Keywords Finder',
-    uis: ['forge'],
-    base: ['sdxl', 'pdxl'],
-    url: 'https://github.com/Avaray/lora-keywords-finder',
+    name: "LoRA Keywords Finder",
+    uis: ["forge"],
+    base: ["sdxl", "pdxl"],
+    url: "https://github.com/Avaray/lora-keywords-finder",
   },
   {
-    name: 'Clip Interrogator',
-    uis: ['forge'],
-    base: ['sdxl'],
-    url: 'https://github.com/pharmapsychotic/clip-interrogator',
-  },
-  {
-    name: 'WD14 Tagger',
-    uis: ['forge'],
-    base: ['pdxl'],
-    url: 'https://github.com/picobyte/stable-diffusion-webui-wd14-tagger',
+    name: "WD14 Tagger",
+    uis: ["forge"],
+    base: ["pdxl"],
+    url: "https://github.com/picobyte/stable-diffusion-webui-wd14-tagger",
   },
 ];
